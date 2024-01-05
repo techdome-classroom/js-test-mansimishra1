204c23d80d24b553e3d3a7ccb1cf2da3fdcf58d9
Mansi Mishra <94820411+mansimishra1@users.noreply.github.com>
2024-01-05 12:59:23 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,8 +3,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+   
+   
+    const leftSym = [];
+   
+    for (let i = 0; i < s.length; i++) {
+     
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            leftSym.push(s[i]);
+        }
+       
+        else if (s[i] === ')' && leftSym.length !== 0 && leftSym[leftSym.length - 1] === '(') {
+            leftSym.pop();
+        } else if (s[i] === '}' && leftSym.length !== 0 && leftSym[leftSym.length - 1] === '{') {
+            leftSym.pop();
+        } else if (s[i] === ']' && leftSym.length !== 0 && leftSym[leftSym.length - 1] === '[') {
+            leftSym.pop();
+        }
+       
+        else {
+            return false;
+        }
+    }
+    return leftSym.length === 0;
 };
+
 
 module.exports = { isValid };
 
